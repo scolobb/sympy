f07665f1e3705f584215bf95147a868cdb8f6981
Sergiu Ivanov <unlimitedscolobb@gmail.com>
2012-08-02 20:22:28 +0300

    Embeddings: Transition to simple graphs.
    Diagrams are essentially multigraphs.  To be able to list all embeddings,
    we will need to first convert both the pattern and the model to a simple
    graph, then apply Ullman's algorithm (see references in the code) to
    generate all subgraph embeddings, and then, from these embeddings, generate
    the multigraph embeddings.
    
    To apply Ullman's algorithm, we will need to do matrix multiplication
    with adjacency matrices, this is why we set up bidirectional mappings
    between the objects of the diagrams and their indices.
    
    This commit introduces the main magic which will eventually allow
    ``diagram_embeddings`` to work with infinite diagrams.

--- sympy/categories/commutativity.py
+++ sympy/categories/commutativity.py
@@ -17,6 +17,7 @@ J. Association of Computing Machinery, March, 1976, 16, 31--42.
 """
 
 from sympy.categories import Diagram
+from sympy import zeros
 
 def diagram_embeddings(pattern, model):
     """
@@ -38,4 +39,81 @@ def diagram_embeddings(pattern, model):
     [Ullm1976] J. R. Ullman, An Algorithm for Subgraph Isomorphism,
     J. Association of Computing Machinery, March, 1976, 16, 31--42.
     """
-    pass
+    def build_obj_idx(diagram):
+        """
+        Returns a tuple, which a mapping between objects and numeric
+        indices and a mapping between numeric indices and objects
+        (i.e., a list of objects).
+        """
+        # Since we only care that object indices are stable during the
+        # runtime of this function, we don't really need any special
+        # ordering on the objects of the diagram.
+        obj_idx = list(diagram.objects)
+
+        idx_obj = {}
+        for i in xrange(len(obj_idx)):
+            idx_obj[obj_idx[i]] = i
+
+        return obj_idx, idx_obj
+
+    pattern_idx_obj, pattern_obj_idx = build_obj_idx(pattern)
+    model_idx_obj, model_obj_idx = build_obj_idx(model)
+
+    pattern_adj_matrix = zeros(len(pattern.objects))
+    model_adj_matrix = zeros(len(model.objects))
+
+    def build_adj_matrix_edge_morphisms(diagram, obj_idx, adj_matrix,
+                                        only_generators):
+        """
+        Fills in the adjacency matrix ``adj_matrix`` and returns the
+        corresponding *_edge_morphisms mapping.
+
+        If ``only_generators == True``, it builds the adjacency matrix
+        based on the generators of the diagram.  Otherwise it uses the
+        ``expanded_generators`` thereof (remember, expanded generators
+        are our main weapon against infinite diagrams).
+
+        An *_edge_morphisms dictionary maps a (directed) edge to the
+        morphisms going between the same two objects in the same
+        direction.  The edge is represented as a pair of object
+        indices.
+        """
+        edge_morphisms = {}
+
+        if only_generators:
+            morphisms = diagram.generators
+        else:
+            morphisms = diagram.expanded_generators
+
+        # We only work with finite diagrams here, this is why we can
+        # simply iterate over all morphisms.
+        for m in morphisms:
+            dom_idx = obj_idx[m.domain]
+            cod_idx = obj_idx[m.codomain]
+
+            adj_matrix[dom_idx, cod_idx] = 1
+
+            edge = (dom_idx, cod_idx)
+            if edge in edge_morphisms:
+                edge_morphisms[edge].append(m)
+            else:
+                edge_morphisms[edge] = [m]
+
+        return edge_morphisms
+
+    # We only consider the generators of ``model`` because all other
+    # morphisms should be mapped in full accordance with how the
+    # generators are mapped.  Thus, we will construct all embeddings
+    # of the multigraph defined by the _generators of the pattern_
+    # into the multigraph defined by the _expanded generators of the
+    # model_.  This will guarantee that all other morphisms of the
+    # pattern will have the necessary counterparts in the model
+    # (because any diagram contains all compositions of all morphisms
+    # it contains).  We will have to check the properties explicitly
+    # though, because they can be explicitly specified for composites
+    # and can thus be different from the intersection of properties of
+    # the components.
+    pattern_edge_morphisms = build_adj_matrix_edge_morphisms(
+        pattern, pattern_obj_idx, pattern_adj_matrix, only_generators=True)
+    model_edge_morphisms = build_adj_matrix_edge_morphisms(
+        model, model_obj_idx, model_adj_matrix, only_generators=False)
