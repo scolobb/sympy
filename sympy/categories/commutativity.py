2ddddd213afaee25a08d7901e103a04c5582a235
Sergiu Ivanov <unlimitedscolobb@gmail.com>
2012-08-04 18:34:04 +0300

    Embeddings: Implement Ullman's algorithm.
    This implementation follows [Ullm1976] as closely as it would make
    sense.  Note, however, that since we are actually dealing with directed
    multigraphs, there are deviations.  The code abounds in comments,
    though.

--- sympy/categories/commutativity.py
+++ sympy/categories/commutativity.py
@@ -17,7 +17,7 @@ J. Association of Computing Machinery, March, 1976, 16, 31--42.
 """
 
 from sympy.categories import Diagram
-from sympy import zeros
+from sympy import zeros, Matrix
 
 def diagram_embeddings(pattern, model):
     """
@@ -38,6 +38,8 @@ def diagram_embeddings(pattern, model):
 
     [Ullm1976] J. R. Ullman, An Algorithm for Subgraph Isomorphism,
     J. Association of Computing Machinery, March, 1976, 16, 31--42.
+
+    [???] TODO: Add a reference to the formal proof.
     """
     def build_obj_idx(diagram):
         """
@@ -139,6 +141,7 @@ def diagram_embeddings(pattern, model):
     # `M_0` is set up similarly to the instructions for digraphs in
     # [Ullm1976], p 9.  The difference arises because [Ullm1976] talks
     # about digraphs, while finite diagrams are directed multigraphs.
+    # See ``subgraph_isomorphisms`` for further details.
     npattern = len(pattern.objects)
     nmodel = len(model.objects)
     M_0 = zeros(npattern, nmodel)
@@ -155,3 +158,128 @@ def diagram_embeddings(pattern, model):
                 # "over" the vertex ``v_m`` such that all of ``v_p``'s
                 # edges math some of ``v_m``'s edges.
                 M_0[v_p, v_m] = 1
+
+    def is_subgraph_isomorphism(M, A, B):
+        """
+        Given the adjacency matrix of the pattern (``A``), of the
+        model (``B``) and a matrix ``M``, checks whether ``M`` defines
+        an embedding of the graph defined by ``A`` into the graph
+        defined by ``B``.
+
+        In other words, this function checks condition (1) from
+        [Ullm1976].  Note that, as different from this source, in
+        computing `C`, _one more_ transposition is made.  In other
+        words, this function actually computes the _transposed_
+        version of the `C` shown in [Ullm1976].  The reason is
+        formally explained in [???].
+        """
+        C = (M * (M * B).transpose()).transpose()
+
+        for i in xrange(A.rows):
+            for j in xrange(A.cols):
+                if (A[i, j] == 1) and (C[i, j] == 0):
+                    return False
+        return True
+
+    # Note that while this code follows [Ullm1976] as closely as
+    # possible, the correspondence between the steps in the article
+    # and the steps in this function should not be considered
+    # strict.
+
+    # Step 1.
+
+    # This vector will describe which vertices have already been
+    # mapped to.  ``F[i] == True`` means that some vertex of the
+    # pattern has already been mapped to the vertex ``i`` of the
+    # model.
+    F = [False] * nmodel
+
+    def subgraph_isomorphisms(M, d, F):
+        """
+        Applies Ullman's algorithm for subgraph isomorphisms.  This
+        function returns a generator which will enumerate all matrices
+        `M'` representing all possible subgraph isomorphisms
+        (embeddings).
+
+        This is the implementation of the very first algorithm shown
+        in [Ullm1976], slightly modified to fit naturally with the
+        recursion.  The sequence of steps Ullman describes is actually
+        a simulation of recursion and a couple loops, using a very
+        basic goto-based language abstraction.
+
+        A fundamental notion in Ullman's algorithm is the matrix `M`,
+        which represents a mapping from the set of vertices of the
+        pattern into the set of vertices of the model.  `M_{ij} = 1`
+        if vertex `i` of the pattern is mapped to vertex `j` of the
+        model.
+
+        ``M`` represents the matrix `M`, which is processed line by
+        line, to eventually construct the matrix `M'`.  One recursive
+        call tries all possible configurations for a line of `M`, and,
+        for each such configuration, spawns a new recursive call,
+        which processes the next line.  When the bottom of the matrix
+        is reached, this function checks whether `M'` is indeed an
+        embedding.
+
+        ``d`` corresponds to `d` in [Ullm1976] and represents the
+        index of the line which is being processed in this call.  The
+        array ``F`` corresponds to `F`.  ``M_d`` is not an array, as
+        in [Ullm1976], but is instead a simple local variable.  We
+        assure the coexistence of the necessary amount of `M_d` values
+        by reallocating ``M_d``'s at each recursive call.
+
+        `H_d` from [Ullm1976] is not represented by anything.  Note
+        that, in the article, there is only one instance of `k`.  When
+        the algorithm moves from line `d` to line `d+1` (a recursive
+        call, in this implementation), it needs to store the value of
+        `k` at which is stopped when it was processing line `d`;
+        that's what `H_d` is used for.  In this implementation, it is
+        not necessary, because ``k`` is an individual variable, local
+        to each new recursive call.
+        """
+        # Step 2.
+
+        # This is actually just a copy of ``M``.
+        M_d = Matrix(M)
+
+        # Step 3.
+        for k in xrange(0, nmodel):
+            if (M[d, k] == 0) or F[k]:
+                # Either mapping the vertex ``d`` of the pattern to
+                # the vertex ``k`` of the model is impossible, or
+                # vertex ``k`` has already been mapped to.
+                continue
+
+            # In the current line (``d``), set all elements save
+            # ``M[d, k]`` to 0.
+            for j in xrange(0, k):
+                M[d, j] = 0
+            for j in xrange(k + 1, nmodel):
+                M[d, j] = 0
+
+            # Step 4, the else part of the sentence.
+            if d == npattern - 1:
+                # We have just completed another matrix, which might be an
+                # isomorphism.  Let's check that (cf. condition (1) in
+                # [Ullm1976]).
+                if is_subgraph_isomorphism(
+                    M, pattern_adj_matrix, model_adj_matrix):
+                    yield M
+            else:
+                # Step 6.
+                F[k] = True
+
+                # We need to explicitly re-yield the results from that
+                # recursive generator.
+                for M in subgraph_isomorphisms(M, d + 1, F):
+                    yield M
+
+                F[k] = False
+
+            # Step 5.
+
+            # Try to set another column in this row to zero.
+            M = Matrix(M_d)
+
+        # Step 7.
+        return
